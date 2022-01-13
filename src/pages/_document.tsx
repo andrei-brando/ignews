7f8da60c49b2a16c8be0bc76e419c61879cfccd8
andrei-brando <albertbrando@hotmail.com>
2022-01-13 15:25:14 -0300

    adiciona comentario

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 
+// este arquivo é como se fosse o index.html de uma aplicação react normal
 export default class MyDocument extends NextDocument {
   render() {
     return (
